621ce97439bcef7334d6a4c06bb1b994f5061d24
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-06-13 21:58:02 +0530

    Project initialized

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"os"
+
+	"github.com/fatih/color"
+)
+
+var red = color.New(color.FgRed, color.Bold)
+
+/* red.Add(color.Bold) */
+
+var green = color.New(color.FgGreen, color.Bold)
+
+/* green.Add(color.Bold) */
+
+var cyan = color.New(color.FgCyan, color.Bold)
+
+/* cyan.Add(color.Bold) */
+
+func info() {
+	cyan.Println("[COMMANDS]")
+	cyan.Println("1. -V --version - current version of GELP")
+}
+
+func validTech(tech string) bool {
+	if tech == "react" || tech == "npm" || tech == "go" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	/* link := cyan.Add(color.Underline) */
+	args := os.Args
+
+	if len(args) == 1 {
+		green.Println("Welcome to GELP!")
+		red.Printf("\n")
+		info()
+	} else {
+		for idx, com := range args {
+			if idx >= 1 {
+				switch com {
+				case "--version", "-V":
+					cyan.Println("GELP V1.0.0")
+				case "--tech", "-T":
+					if idx+1 >= len(args) {
+						red.Println("VALUE MISSING...")
+					} else {
+						value := args[idx+1]
+						if value[0] == '-' {
+							red.Println("BAD SYNTAX...")
+						} else if validTech(value) {
+							green.Println(value)
+						} else {
+							red.Println("TECH NOT RECOGNIZED...")
+							info()
+						}
+					}
+				case "react", "npm", "go":
+					check := false
+					for _, flag := range args {
+						if flag == "-T" || flag == "--tech" {
+							check = !check
+						}
+					}
+					if !check {
+						red.Println("FLAG MISSING...")
+						info()
+					}
+				default:
+					red.Println("BAD COMMAND...")
+				}
+			}
+		}
+	}
+}
